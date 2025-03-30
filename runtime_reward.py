635ecfe3b742165b74d6ec17d8a31866d62bd38f
John Cashy <jtcashy@umich.edu>
2025-03-30 12:20:32 -0400

    fix reward

--- runtime_reward.py
+++ runtime_reward.py
@@ -7,13 +7,15 @@ class RuntimeImprovementWrapper(RewardWrapper):
 
     def reset(self, **kwargs):
         obs = self.env.reset(**kwargs)
+        # Get the initial runtime
         self.last_runtime = self.env.observation["Runtime"]
         return obs
 
-    def convert_reward(self, reward):  # required by CompilerGym's RewardWrapper
+    def convert_reward(self, reward):  # required method
+        # Get current runtime
         current_runtime = self.env.observation["Runtime"]
-        if self.last_runtime is None:
+        if self.last_runtime is None or current_runtime is None:
             return 0.0
-        reward = self.last_runtime - current_runtime  # reward improvement
+        reward = self.last_runtime - current_runtime
         self.last_runtime = current_runtime
         return reward
