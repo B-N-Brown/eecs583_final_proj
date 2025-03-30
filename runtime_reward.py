a6a95ecf17aaa1887ec73145329457820532d47f
John Cashy <jtcashy@umich.edu>
2025-03-30 12:01:34 -0400

    fix

--- runtime_reward.py
+++ runtime_reward.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from compiler_gym.wrappers import RewardWrapper
 
 class RuntimeImprovementWrapper(RewardWrapper):
@@ -18,33 +17,3 @@ class RuntimeImprovementWrapper(RewardWrapper):
         reward = self.last_runtime - current_runtime  # reward improvement
         self.last_runtime = current_runtime
         return reward
-=======
-from compiler_gym.spaces import Reward
-
-class RuntimeReward(Reward):
-    """An example reward that uses changes in the "runtime" observation value
-    to compute incremental reward.
-    """
-
-    baseline_runtime: int
-
-    def __init__(self):
-        super().__init__(
-            name="runtime",
-            observation_spaces=["runtime"],
-            default_value=0,
-            default_negates_returns=True,
-            deterministic=False,
-            platform_dependent=True,
-        )
-        self.baseline_runtime = 0
-
-    def reset(self, benchmark: str, observation_view):
-        del benchmark  # unused
-        self.baseline_runtime = observation_view["runtime"]
-
-    def update(self, action, observations, observation_view):
-        del action  # unused
-        del observation_view  # unused
-        return float(self.baseline_runtime - observations[0]) / self.baseline_runtime
->>>>>>> 8f8e6a3c06354f42f4985215a1f127ee2dafe719
