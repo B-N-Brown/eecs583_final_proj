afae538d1c02dec7cd0ba8feaf7b9afb5b684711
Rahul Ramesh <rahulramesh@Rahuls-MacBook-Air.local>
2025-04-15 18:44:45 -0400

    testing change

--- train_unroll_model.py
+++ train_unroll_model.py
@@ -3,6 +3,6 @@ from stable_baselines3.common.env_util import make_vec_env
 
 from loop_unroll_env import LoopUnrollEnv
 
-gym_env = make_vec_env(LoopUnrollCompilerGymEnv, n_envs=4)
+gym_env = make_vec_env(LoopUnrollEnv, n_envs=4)
 model = PPO("MlpPolicy", gym_env, verbose=1)
 model.learn(total_timesteps=20000)
