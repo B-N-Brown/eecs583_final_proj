bf492a4d12473924a9ba9c538e557c98deb8e6f6
rmsh <rmsh@umich.edu>
2025-04-15 19:11:11 -0400

    fixed error message

--- loop_unroll_env.py
+++ loop_unroll_env.py
@@ -28,8 +28,8 @@ class LoopUnrollEnv(gym.Env):
         try:
             self.env.write_ir(passes=passes)
             reward = -self.env.observation["Runtime"]
-        except Exception:
-            print(Exception)
+        except Exception as e:
+            print(e)
             # Fallback if LLVM errors
             reward = -1e6
         obs = self.env.observation["Autophase"]
