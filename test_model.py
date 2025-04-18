ee1eeb2799c8cd80abc405de66dc5ae2dc39f47c
bnb <bnb@localhost.localdomain>
2025-04-18 16:54:28 -0400

    Some inference code

--- test_model.py
+++ test_model.py
@@ -1,17 +1,84 @@
 import gym
+import torch
 import compiler_gym
 import numpy as np
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 from compiler_gym.wrappers import RewardWrapper
 
 from runtime_reward import RuntimeImprovementWrapper
 
+import stable_baselines3
+from stable_baselines3 import PPO, A2C
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.env_util import make_vec_env
+
+from loop_actions import loop_opt_actions, loop_action_space
 
 
-def test_model(env, checkpoint):
+def test_model(env, checkpoint_name="basic_model.pth"):
     # model = model.load(path=f"model_checkpoints/{checkpoint_name}")
-    return
 
+    model = PPO.load(f"model_checkpoints/{checkpoint_name}", 
+                     print_system_info=True)
+    
+    averaged_reward = evaluate_policy(
+                            env=env, 
+                            model=model,
+                            n_eval_episodes=1,
+                            reward_threshold=0.0000001,
+                            return_episode_rewards=True,              
+                     ) 
+
+    print("Averaged reward:", averaged_reward)
+
+def test_model_loop(env, checkpoint_name="basic_model.pth"):
+    # benchmarks = env.datasets["cbench-v1/sha"].benchmarks
+    results = []
+
+    model = PPO.load(f"model_checkpoints/{checkpoint_name}", 
+                     print_system_info=True)
+
+
+    # for benchmark in benchmarks[:10]:
+    #     obs = env.reset(benchmark=benchmark)
+    #     done = False
+    #     total_reward = 0
+
+    #     while not done:
+    #         action, _ = model.predict(obs, deterministic=True)
+    #         obs, reward, done, info = env.step(action)
+    #         total_reward += reward
+
+    #     results.append((benchmark, total_reward, info))
+
+
+    n_episodes = 1 
+    # env = gym.wrappers.RecordEpisodeStatistics(env, n_episodes)
+    done = False
+
+    for episode in tqdm(range(n_episodes)):
+        env.reset()
+        total_reward = 0
+
+        with tqdm(desc="Processing") as pbar:
+            tqdm_counter = 0
+
+            while not done:
+                action = env.action_space.sample()
+
+                observation, reward, done, info = env.step(action)
+                total_reward += reward
+                
+                tqdm_counter += 1
+                pbar.update(1)
+            
+            # exit()
+
+        results.append(total_reward)
+
+    plt.plot(total_reward)
+    plt.savefig("test_plot.png")
 
 def main():
     env = compiler_gym.make(
@@ -23,11 +90,16 @@ def main():
     env = RuntimeImprovementWrapper(env)
     env.runtime_observation_count = 1
 
+    env.action_space = loop_action_space
+
+    env = RuntimeImprovementWrapper(env)
+    env.reset()
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("USING DEVICE:", device)
 
     # Testing code
-    ppo_training_sb(env, device)
+    test_model_loop(env)
 
 
 
