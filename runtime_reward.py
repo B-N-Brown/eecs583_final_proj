bfddc286dab2596c3cff28ace18ead29a303da3c
Rahul Ramesh <rr6004@gmail.com>
2025-04-18 20:23:24 -0400

    working model with threshold

--- runtime_reward.py
+++ runtime_reward.py
@@ -10,26 +10,30 @@ class RuntimeImprovementWrapper(RewardWrapper):
     def reset(self, **kwargs):
         obs = self.env.reset(**kwargs)
         # Get the initial runtime
-        self.last_runtime = self.env.observation["Runtime"]
+        self.last_runtime = np.mean(self.env.observation["Runtime"])
         return obs
     
     def step(self, action, observation=None):
         # action = int(action)
         observation, reward, done, info = self.env.step(action)
         reward = self.convert_reward(reward)
+        print("reward ratio: ", self.reward_ratio)
+        done = self.reward_ratio < 0.1
         return observation, reward, done, info 
 
     def convert_reward(self, reward):  # required method
         # Get current runtime
         current_runtime = self.env.observation["Runtime"]
+        current_runtime = np.mean(np.array(current_runtime))
+
         if self.last_runtime is None or current_runtime is None:
             return 0.0
+
         reward = self.last_runtime - current_runtime
+        self.reward_ratio = abs(reward)/self.last_runtime
         self.last_runtime = current_runtime
 
         # For compatibility with stable baselines
         # convert to np array and average runtime observation count
-        # TODO: put this before the array subtraction
-        reward = np.mean(np.array(reward))
-
+        
         return reward
--- test_model.py
+++ test_model.py
@@ -5,6 +5,7 @@ import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from compiler_gym.wrappers import RewardWrapper
+from compiler_gym.spaces.commandline import Commandline, CommandlineFlag
 
 from runtime_reward import RuntimeImprovementWrapper
 
@@ -35,55 +36,67 @@ def test_model(env, checkpoint_name="basic_model.pth"):
 def test_model_loop(env, checkpoint_name="basic_model.pth"):
     # benchmarks = env.datasets["cbench-v1/sha"].benchmarks
     total_rewards = []
+    initial_execution_times = []
+    final_execution_times = []
     prediction_times = []
+    action_selction_freq = {} 
+
+    empty_flag = CommandlineFlag(" ", " ", "empty_flag")
 
     model = PPO.load(f"model_checkpoints/{checkpoint_name}", 
                      print_system_info=True)
-
-
-    # for benchmark in benchmarks[:10]:
-    #     obs = env.reset(benchmark=benchmark)
-    #     done = False
-    #     total_reward = 0
-
-    #     while not done:
-    #         action, _ = model.predict(obs, deterministic=True)
-    #         obs, reward, done, info = env.step(action)
-    #         total_reward += reward
-
-    #     results.append((benchmark, total_reward, info))
 
 
     n_episodes = 1 
     # env = gym.wrappers.RecordEpisodeStatistics(env, n_episodes)
     done = False
 
-    for episode in tqdm(range(n_episodes)):
+    for episode in (range(n_episodes)):
         env.reset()
         total_reward = 0
         observation = env.reset()
+        print("obs:", observation)
 
-        with tqdm(desc="Processing") as pbar:
-            tqdm_counter = 0
+        # Getting baselines run
+        # env.action_space = empty_flag
+        # fake_action = env.sam
+        # env.step
 
-            while not done:
-                action, _ = model.predict(observation, deterministic=True)
-                print("ACTION:", type(action))
-                action = env.action_space.sample()
-                print("ACTION1:", type(action))
+        # with tqdm(desc="Processing") as pbar:
+        #     tqdm_counter = 0
 
-                observation, reward, done, info = env.step(action)
+        while not done:
+            print("iterating through done loop")
+            action, _ = model.predict(observation, deterministic=False)
+            action = int(action.tolist())
+            
+            #print("SAMPLD ACTION:", env.action_space.to_string(action))
 
-                print("SAMPLD ACTION:", env.action_space.to_string(action))
-                print("info: ", info)
+            # action = env.action_space.sample()
+            # print("ACTION:", action)
 
+            observation, reward, done, info = env.step(action)
 
-                print("ACTION:", action)
+            # print("INFO:", info)
+            # print("REWARD: ", reward)
 
-                total_reward += reward
+            if not info["action_had_no_effect"]:
+                action_name = env.action_space.to_string(action)
                 
-                tqdm_counter += 1
-                pbar.update(1)
+                if action_name not in action_selction_freq:
+                    action_selction_freq[env.action_space.to_string(action)] = 0
+
+                else:
+                    action_selction_freq[env.action_space.to_string(action)] += 1
+
+            total_reward += reward
+                
+                # tqdm_counter += 1
+                # pbar.update(1)
+            print()
+            break
+
+                # Done threshold
             
             # exit()
 
@@ -91,8 +104,8 @@ def test_model_loop(env, checkpoint_name="basic_model.pth"):
         total_rewards.append(total_reward)
         prediction_times.append(env.episode_walltime)
 
-    plt.plot(total_reward)
-    plt.savefig("test_plot.png")
+    # plt.plot(total_reward)
+    # plt.savefig("test_plot.png")
 
 def main():
     env = compiler_gym.make(
@@ -101,8 +114,6 @@ def main():
         observation_space="Autophase",
         reward_space="IrInstructionCountOz"
     )
-    env = RuntimeImprovementWrapper(env)
-    env.runtime_observation_count = 1
 
     env.action_space = loop_action_space
 
